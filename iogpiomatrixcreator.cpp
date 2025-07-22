6688fd7cd8e5e7f47fe94f38d38ff050808d47fc
hackboxguy <albert.david@gmail.com>
2025-07-22 15:57:05 +0200

    changed the pi4 gpio pin mapping of jtag signals

--- iogpiomatrixcreator.cpp
+++ iogpiomatrixcreator.cpp
@@ -1,7 +1,13 @@
 #include "iogpiomatrixcreator.h"
 
 IOGPIOMatrixCreator::IOGPIOMatrixCreator()
- : IOGPIODPi(4, 17, 27, 22)
+ //: IOGPIODPi(4, 17, 27, 22)
+ : IOGPIODPi(26, 16, 21, 20)
 {
+    // Pin mapping (in order: TMS, TCK, TDI, TDO):
+    // GPIO4  -> TMS (Mode Select)
+    // GPIO17 -> TCK (Clock)
+    // GPIO27 -> TDI (Data In)
+    // GPIO22 -> TDO (Data Out)
 }
 
