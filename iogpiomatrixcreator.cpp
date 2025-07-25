6a6580a2adf14e815c40f63455b9085520e98460
hackboxguy <albert.david@gmail.com>
2025-07-25 23:44:19 +0200

    changed the jtag pins of pi4 to 26, 16, 21, 20

--- iogpiomatrixcreator.cpp
+++ iogpiomatrixcreator.cpp
@@ -2,8 +2,8 @@
 
 IOGPIOMatrixCreator::IOGPIOMatrixCreator()
  //orig: IOGPIODPi(4, 17, 27, 22)
- //slow: IOGPIODPi(26, 16, 21, 20)
- : IOGPIODPi(25, 11, 10, 9)
+ : IOGPIODPi(26, 16, 21, 20)
+ //: IOGPIODPi(25, 11, 10, 9)
 {
     // Pin mapping (in order: TMS, TCK, TDI, TDO):
     // GPIO4  -> TMS (Mode Select)
